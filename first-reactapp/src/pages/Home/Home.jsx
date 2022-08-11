f040943419f6fa1660ab32978b8a6dea166689f8
Yan <yanfornimedeiros@gmail.com>
2022-08-11 14:11:00 -0300

    Entendendo um pouco sobre estado

--- first-reactapp/src/pages/Home/Home.jsx
+++ first-reactapp/src/pages/Home/Home.jsx
@@ -1,11 +1,25 @@
-import './style.css'
+import React, {useState} from 'react';
+
+import "./style.css";
+
+import { Card } from "../../components/Card";
 
 export function Home() {
+
+  const [studentName, setStudentName] = useState()
+
   return (
     <div className="container">
-    <h1>Lista de Presença</h1>
-    <input type="text" placeholder="Digite um nome"/>
-    <button type="button">Adicionar</button>
+      <h1>Lista de Presença {studentName}</h1>
+      <input 
+      type="text" 
+      placeholder="Digite um nome"
+      onChange={e => setStudentName(e.target.value)}/>
+      <button type="button">Adicionar</button>
+
+      <Card name="Rodrigo" time="10:10:10" idade={26} />
+      <Card name="Josias" time="22:10:10" idade={27} />
+      <Card name="Josefina" time="22:15:49" idade={27} />
     </div>
-  )
+  );
 }
