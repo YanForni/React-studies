8fcc29dbe0ea84eb874c607840959de567420fbb
Yan <yanfornimedeiros@gmail.com>
2022-08-11 10:00:22 -0300

    Renomeando alguns itens

--- first-reactapp/src/pages/Home/Home.jsx
+++ first-reactapp/src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
-function App() {
+import './style.css'
+
+export function Home() {
   return (
-    <>
+    <div className="container">
     <h1>Lista de Presença</h1>
     <input type="text" placeholder="Digite um nome"/>
     <button type="button">Adicionar</button>
-    </>
+    </div>
   )
 }
-
-export default App
