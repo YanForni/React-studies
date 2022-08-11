f76b02922e9396e534a87f67aa1f09306ae4186d
Yan <yanfornimedeiros@gmail.com>
2022-08-11 09:04:00 -0300

    Movendo para pasta src

--- first-reactapp/src/main.jsx
+++ first-reactapp/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './Home'
+import Home from './pages/Home/Home'
+import './styles/global.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
