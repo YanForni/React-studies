943963ecadc5eaf5fb266c43690cbd65f2fe454c
Yan <yanfornimedeiros@gmail.com>
2022-08-12 16:27:13 -0300

    Removendo counter

--- github-explorer/src/components/App.jsx
+++ github-explorer/src/components/App.jsx
@@ -1,12 +1,10 @@
 import '../style/global.scss';
-import { Counter } from './Counter';
 import { RepositoryList } from './RepositoryList';
 
 export function App() {
     return (
         <div>
             <RepositoryList />
-            <Counter />
         </div>
     )
 }
