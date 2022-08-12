f2adb36a6cd9193b8ae33435d6725e9ac93965ac
Yan <yanfornimedeiros@gmail.com>
2022-08-12 17:52:44 -0300

    Consumindo API do github

--- github-explorer/src/components/RepositoryList.jsx
+++ github-explorer/src/components/RepositoryList.jsx
@@ -1,19 +1,35 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+import '../style/repositories.scss'
+import { useEffect, useState } from "react";
+
+//https://api.github.com/users/YanForni/repos
+
 const repository = {
     name: 'Repo',
-    description: 'Repo of react',
+    description: 'Repo of react', 
     link: 'https://github.com/'
 }
-
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([])  
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/YanForni/repos")
+    .then(response => response.json())
+    .then(data => setRepositories(data))
+  
+  }, [])
+  
+
   return (
-    <section>
+    <section className="repository-list">
       <h1>Lista de Repositórios</h1>
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
+        
+        
       </ul>
     </section>
   );
