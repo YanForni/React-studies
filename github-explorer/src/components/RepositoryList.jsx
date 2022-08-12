24b689efd3263131b206937051e82fcb78406480
Yan <yanfornimedeiros@gmail.com>
2022-08-12 14:20:24 -0300

    Criando componente RepositoryList

---
+++ github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,36 @@
+const repositoryName = "Unform2"
+
+export function RepositoryList(){
+    return (
+        <section>
+            <h1>Lista de Repositórios</h1>
+            <ul>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+                    <a href="#">
+                        Acessar repositórios
+                    </a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+                    <a href="#">
+                        Acessar repositórios
+                    </a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+                    <a href="#">
+                        Acessar repositórios
+                    </a>
+                </li>
+            </ul>
+        </section>
+    )
+}
