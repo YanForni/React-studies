a420c5179ee7e6684332428beff5730416ada2d2
Yan Forni <109802374+YanForni@users.noreply.github.com>
2022-08-17 18:00:08 -0300

    Modal

--- dt-money/my-app/src/App.tsx
+++ dt-money/my-app/src/App.tsx
@@ -3,13 +3,31 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./style/global";
+import Modal from 'react-modal';
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+          <NewTransactionModal 
+          isOpen= {isNewTransactionModalOpen}
+          onRequestClose= {handleCloseNewTransactionModal}
+
+          />
       <GlobalStyle />
     </>
   );
