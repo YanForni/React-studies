4fe2565a969e01547475663e7cce898a5b008c48
Yan Forni <yanfornimedeiros@gmail.com>
2022-09-19 23:45:02 -0300

    Adicionando transações de exemplo

--- dt-money/my-app/src/index.tsx
+++ dt-money/my-app/src/index.tsx
@@ -1,30 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 
 createServer({
+
+  models:{
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transaction: [
+        {
+          id: 1,
+          title: 'Freelance de um site',
+          type: 'deposit',
+          category: 'dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 9:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'casa',
+          amount: 1300,
+          createdAt: new Date('2021-02-21 10:00:00')
+        },
+      ]
+    })
+  }
+
   routes() {
     this.namespace= 'api';
     
     this.get('/transactions', ()=> {
-      return [
-        {
-          id: 1,
-          title: 'Transaction',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transactions');
     })
 
     this.post('/transactions', (schema, request)=> {
       const data = JSON.parse(request.requestBody)
 
-      return data
+      return schema.create('transaction', data)
     })
   }
 })
