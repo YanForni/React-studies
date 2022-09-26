d6f0b956f83828e0ed3f198d06b137b11c7a974c
Yan Forni <yanfornimedeiros@gmail.com>
2022-09-26 16:16:42 -0300

    Trocando TransactionContext por hook criado

--- dt-money/my-app/src/hooks/useTransactions.tsx
+++ dt-money/my-app/src/hooks/useTransactions.tsx
@@ -1,6 +1,6 @@
 import { type } from "os";
-import { createContext, ReactNode, useEffect, useState } from "react";
-import { api } from "./services/api";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { api } from "../services/api";
 
 interface Transaction {
   id: number;
@@ -66,4 +66,10 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       {children}
     </TransactionsContext.Provider>
   );
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+
+  return context;
 }
